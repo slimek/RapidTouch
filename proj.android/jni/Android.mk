66592bded81413cd3ee0391c2f2754608ac8bfa1
slimek <slime573@gmail.com>
2013-11-20 22:52:41 +0800

    [NDK] Implements Android.mk (by include Toffee/PrebuiltLibs.mk).

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -4,6 +4,8 @@ RT_LOCAL_PATH := $(call my-dir)
 # Include Pre-built Libraries
 #
 
+include $(GIT_ROOT)/Toffee/proj.android/jni/PrebuiltLibs.mk
+
 
 #
 # Build RapidTouch
@@ -18,15 +20,21 @@ LOCAL_MODULE := RapidTouch
 RT_SRC := ../../src
 
 LOCAL_SRC_FILES := \
-	$(RT_SRC)/LogScene.cpp \
+	$(RT_SRC)/LogoScene.cpp \
 	$(RT_SRC)/RapidTouchApp.cpp \
 	$(RT_SRC)/RapidTouchPch.cpp
 
 LOCAL_C_INCLUDES := \
+	../../src \
+	$(TOFFEE_C_INCLUDES)
 
 LOCAL_LDLIBS := \
+	$(TOFFEE_LDLIBS)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := \
+    Toffee \
+    Caramel \
+	$(TOFFEE_STATIC_LIBRARIES)
 
 include $(BUILD_SHARED_LIBRARY)
 
