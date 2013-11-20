b5350ce825c0605d74fa3215e1eb8136957268d2
slimek <slime573@gmail.com>
2013-11-20 23:30:35 +0800

    [NDK] Add src.android and RapidTouch.java

---
+++ src.android/com/toffee/rapidtouch/RapidTouch.java
@@ -0,0 +1,17 @@
+package com.toffee.rapidtouch;
+
+import org.cocos2dx.lib.Cocos2dxActivity;
+import android.os.Bundle;
+
+
+public class RapidTouch extends Cocos2dxActivity {
+
+	
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+	}
+	
+	static {
+		System.loadLibrary( "RapidTouch" );
+	}
+}
