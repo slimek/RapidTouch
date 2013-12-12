b2c21c2de4de41800ad28872a8978b1ec7113679
slimek <slime573@gmail.com>
2013-12-12 23:38:38 +0800

    [NDK] APP_CPPFLAGS doesn't need to duplicate APP_CFLAGS.

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -2,6 +2,6 @@ APP_PLATFORM := android-10
 APP_ABI := x86 armeabi
 APP_STL := gnustl_static
 APP_CFLAGS := -g -fexceptions -DCOCOS2D_DEBUG=1
-APP_CPPFLAGS := -std=c++11 -frtti $(APP_CFLAGS)
+APP_CPPFLAGS := -std=c++11 -frtti
 
 NDK_TOOLCHAIN_VERSION := 4.8
